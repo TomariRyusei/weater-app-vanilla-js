d3db24af4504b8582dbddeeabf65303946f76f8c
TomariRyusei <t.ryu6635@gmail.com>
2023-01-29 16:50:55 +0900

    HTML and CSS layout and call weather api

---
+++ weather.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+//https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&windspeed_unit=ms&timeformat=unixtime
+
+export function getWeather(latitude, longitude, timezone) {
+  return axios.get(
+    "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&windspeed_unit=ms&timeformat=unixtime",
+    {
+      params: {
+        latitude: latitude,
+        longitude: longitude,
+        timezone: timezone,
+      },
+    }
+  );
+}
